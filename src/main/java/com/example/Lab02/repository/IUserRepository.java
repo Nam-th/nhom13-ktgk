cb826ddc02b78c02325bfefed1ea9223e9bda21b
Nam-th <th.nam1112@gmail.com>
2024-06-12 11:26:11 +0700

    feat: Authentication

---
+++ src/main/java/com/example/Lab02/repository/IUserRepository.java
@@ -0,0 +1,9 @@
+package com.example.Lab02.repository;
+import com.example.Lab02.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+@Repository
+public interface IUserRepository extends JpaRepository<User, String>{
+    Optional<User> findByUsername(String username);
+}
